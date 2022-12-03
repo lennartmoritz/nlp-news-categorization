faf393497d42101e6fa457f2d42ddc1668449d48
lennartmoritz <lennartmoritz26@googlemail.com>
2022-12-03 17:31:05 +0100

    refactor everything a little bit

--- news-categorization.py
+++ news-categorization.py
@@ -10,14 +10,12 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
-from torchmetrics import Accuracy
-from torchmetrics import Recall
-from torchmetrics import Precision
-from torchmetrics import F1Score
+from torchmetrics import Accuracy, Recall, Precision, F1Score
+import sys
 
 
 # Split the dataset into sub-set of specific category
-def data_splitter(dataset, key, value):
+def data_splitter(dataset, key, value) -> pd.DataFrame:
     grouped_dataset = dataset.groupby(dataset[key])
     sub_set = grouped_dataset.get_group(value)
     return sub_set
@@ -31,67 +29,63 @@ def data_merger(list_of_datasets):
     dataframes_merged = pd.concat(dataframes)
     return dataframes_merged
 
-# TODO: Might play a part in the "Cluster-Fuck-Land"-Issue...
-# Separate dataset into train-data and test-data (Manual version without Loader)
-def get_train_test_data(dataset, fraction):
-    train_dataset = dataset.sample(frac=fraction, random_state=0)
-    test_dataset = dataset.drop(train_dataset.index)
-    return train_dataset, test_dataset
-
 # Generate the word embeddings for the selected dataset
 def get_word_embeddings(dataset):
-        # Getting all the vocabularies and indexing to a unique position
-        vocab = Counter()
-        # Indexing words from the training data
-        for text in dataset['TITLE']:
-            for word in text.split(' '):
-                vocab[word.lower()] += 1
+    """
+    Returns:
+    word_embeddings:    A dict that maps word names as keys to an automatically generated word_id
+    """
+    # Getting all the vocabularies and indexing to a unique position
+    vocab = Counter()
+    # Indexing words from the training data
+    for text in dataset['TITLE']:
+        for word in text.split(' '):
+            vocab[word.lower()] += 1
 
-        word_embeddings = {}
-        for i, word in enumerate(vocab):
-            word_embeddings[word.lower()] = i
+    word_embeddings = {}
+    for i, word in enumerate(vocab):
+        word_embeddings[word.lower()] = i
 
-        return word_embeddings
+    return word_embeddings
 
-# 
 class CustomNewsDataset(Dataset):
-        def __init__(self, data, embeddings, categories):
-            self.labels = data['CATEGORY']
-            self.titles = data['TITLE']
-            self.embeddings = embeddings
-            self.categories = categories
+    def __init__(self, data, embeddings, categories):
+        self.labels = data['CATEGORY']
+        self.titles = data['TITLE']
+        self.embeddings = embeddings
+        self.categories = categories
 
-        def __len__(self):
-            return len(self.titles)
+    def __len__(self):
+        return len(self.titles)
 
-        def __getitem__(self, idx):
-            title = self.titles.iloc[idx]
-            embedded_title = np.zeros(len(self.embeddings), dtype=np.float64)
+    def __getitem__(self, idx):
+        title = self.titles.iloc[idx]
+        embedded_title = np.zeros(len(self.embeddings), dtype=np.float64)
 
-            for word in title.split(' '):
-                embedded_title[self.embeddings[word.lower()]] += 1
+        for word in title.split(' '):
+            embedded_title[self.embeddings[word.lower()]] += 1
 
-            label = self.labels.iloc[idx]
-            label = self.categories.index(label)
-            return torch.Tensor(embedded_title), label
+        label = self.labels.iloc[idx]
+        label = self.categories.index(label)
+        return torch.Tensor(embedded_title), label
             
 # Define the network
 class NewsClassifier(nn.Module):
-        def __init__(self, input_size, hidden_size, num_classes):
-            super().__init__()
-            self.layer_1 = nn.Linear(input_size, hidden_size, bias=True)
-            self.relu = nn.ReLU()
-            self.layer_2 = nn.Linear(hidden_size, hidden_size, bias=True)
-            self.output_layer = nn.Linear(hidden_size, num_classes, bias=True)
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        self.layer_1 = nn.Linear(input_size, hidden_size, bias=True)
+        self.relu = nn.ReLU()
+        self.layer_2 = nn.Linear(hidden_size, hidden_size, bias=True)
+        self.output_layer = nn.Linear(hidden_size, num_classes, bias=True)
 
-        # Accept input and return an output
-        def forward(self, x):
-            out = self.layer_1(x)
-            out = self.relu(out)
-            out = self.layer_2(out)
-            out = self.relu(out)
-            out = self.output_layer(out)
-            return out
+    # Accept input and return an output
+    def forward(self, x):
+        out = self.layer_1(x)
+        out = self.relu(out)
+        out = self.layer_2(out)
+        out = self.relu(out)
+        out = self.output_layer(out)
+        return out
 
 # Safe-guarding the script to prevent errors occuring related to multiprocessing
 if __name__ == "__main__":
@@ -107,15 +101,13 @@ if __name__ == "__main__":
 
     # +++ Binary classifications +++ (all posssible combinations of two)
     # Group data for customized data selection
-    data_all = pd.read_csv("data/uci-news-aggregator.csv") #TODO: Christian-Question: Loading again to not mess with "data" by grouping on it -> Does that make sense/is it necessary?
-    print("Type of data_all: ", type(data_all))
+    data_all = pd.read_csv("data/uci-news-aggregator.csv")
     
     #grouped_dataset = data_all.groupby(data_all['CATEGORY'])
     data_e = data_splitter(data_all, 'CATEGORY', 'e') #grouped_dataset.get_group('e')
     data_b = data_splitter(data_all, 'CATEGORY', 'b')
     data_t = data_splitter(data_all, 'CATEGORY', 't')
     data_m = data_splitter(data_all, 'CATEGORY', 'm')
-    print("Type of data_e: ", type(data_e))
     
     # Sanity-Check
     print("Number of e: " + str(len(data_e)) + "\tNumber of b: " + str(len(data_b)) + "\tNumber of t: " + str(len(data_t)) + "\tNumber of m: " + str(len(data_m)))
@@ -133,52 +125,46 @@ if __name__ == "__main__":
 
     # +++ Multi-class +++ (all four of them together)
     data_all = data_merger([data_e, data_b, data_t, data_m]) # All (e, b, t, m)
-    print("Type of data_all: ", type(data_all))
-
-
-
-    ######################################################################## Cluster-Fuck-Land
-    # TEMP to not interrupt other coder!
-    # !!! data_splitter() is not the same as previously !!!
-    full_train_dataset, full_test_dataset = get_train_test_data(data_all, 0.8)
-    train_dataset = full_train_dataset
-    print("Type of train_dataset: ", type(train_dataset))
-    test_dataset = full_test_dataset
-    ######################################################################## Cluster-Fuck-Land
-
-
-
 
     # Selection of data to be used in the model
     # TODO: Actual selection happening => This might include some restructuring of above data #PythonMagic
     data = data_all                     # | data_eb     | data_et       | ...
-    print("Type of data: ", type(data))
+    unique_categories = len(data['CATEGORY'].unique())
+
     categories = ['e', 'b', 't', 'm']   # | ['e', 'b']  | ['e', 't']    | ...
     
     # Get word embeddings based on currently selected dataset
     embeddings = get_word_embeddings(data)
 
-
     # Dataset based on DataLoader and preselected "data" (which categories?)
     dataset = CustomNewsDataset(data, embeddings, categories)
-    print("Type of dataset: ", type(dataset))
     
     #train_size = int(0.8 * len(dataset))
     train_size = 500 # Sub-set for quick debugging
-        
     #test_size = len(dataset) - train_size
     test_size = 100  # Sub-set for quick debugging    
     
     validate_size = len(dataset) - (train_size + test_size) # Unused sub-set for quick debugging
     
+    #TODO: check that the datasets are balanced, i.e. all categories must appear
     training_dataset, testing_dataset, validating_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, validate_size])
     print("Type of training_dataset: ", type(training_dataset))
+    
+    def check_dataset_balance(this_dataset, intended_num_of_classes) -> bool:
+        """
+        Returns True if intended number of unique classes are present in this dataset
+        """
+        print("Checking that all unique classes are represented...")
+        duplicate_removal_set = set()
+        for item in tqdm(this_dataset):
+            duplicate_removal_set.add(item[1])
+        return len(duplicate_removal_set) == intended_num_of_classes
 
-    ######################################################################## Cluster-Fuck-Land
-    #train_dataset = training_dataset
-    #print("Type of train_dataset: ", type(train_dataset))
-    #test_dataset = testing_dataset
-    ######################################################################## Cluster-Fuck-Land
+    if False:
+        assert check_dataset_balance(training_dataset, unique_categories)
+        assert check_dataset_balance(testing_dataset, unique_categories)
+        assert check_dataset_balance(validating_dataset, unique_categories)
+
     
     # Parameters
     params = {'batch_size': 64,
@@ -191,16 +177,13 @@ if __name__ == "__main__":
     test_generator = DataLoader(testing_dataset, **params)
     #validate_generator = DataLoader(validating_dataset, **params)
 
-    # Parameters TODO: Christian-Question: How come we have batch_size in params and then here again?
     learning_rate = 0.01    # How fast the model learns
     num_epochs = 5          # How often the model walks through the data
-    batch_size = 150        # How big the groups of separated data entries are
-    display_step = 1        # TODO: Is this even used? Also: What is it?
 
     # Network Parameters
     hidden_size = 100  # 1st layer and 2nd layer number of features
     input_size = len(embeddings)  # Words in vocab
-    num_classes = 4  # Categories: "e", "b", "t", "m"
+    num_classes = unique_categories  # Categories: "e", "b", "t", "m"
     # e: entertainment | b: business | t: science and technology | m: health
     # e: 152469        | b: 115967   | t: 108344                 | m: 45639
 
@@ -213,30 +196,11 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()  # This includes the Softmax loss function
     optimizer = torch.optim.Adam(news_net.parameters(), lr=learning_rate)
 
-    print("Type of train_dataset: ", type(train_dataset))
-
     # Train the Model
     for epoch in range(num_epochs):
-        # Shuffle the data
-        train_dataset = train_dataset.sample(frac=1).reset_index(drop=True) # TODO: Christian-Question: Why do we use "train_dataset" and not "training_dataset"? 
-        
-        
-        ######################################################################## Cluster-Fuck-Land
-        # TODO: trying with training_dataset since error states: AttributeError: 'Subset' object has no attribute 'sample'
-        #train_dataset = training_dataset.sample(frac=1).reset_index(drop=True) 
-        #
-        #train_dataset = training_dataset.sample(frac=1).reset_index(drop=True)
-        #
-        #print("Type of training_dataset: ", type(training_dataset))
-        #training_dataset = training_dataset.sample(frac=1).reset_index(drop=True)
-        #
-        # total_batch = int(len(training_dataset) / batch_size)
-        ######################################################################## Cluster-Fuck-Land
-       
-        
         # Determine the number of min-batches based on the batch size and size of training data
-        total_batch = int(len(train_dataset) / batch_size)
         avg_loss = 0
+        total_batch = len(train_generator)
         # Loop over all batches
         for titles, labels in tqdm(train_generator):
             titles, labels = titles.to(device), labels.to(device)
@@ -256,11 +220,6 @@ if __name__ == "__main__":
     # Save model after training
     # TODO: Careful! This gets big really fast!
     #torch.save(news_net, "./models/currentModel")
-    
-
-    
-    ############################################################################
-    # TODO: Move evaluation parts to new script [news_evaluation.py]
     
     # Calculate Accuracy
     news_net.eval()
@@ -282,82 +241,41 @@ if __name__ == "__main__":
     labels_unique = (torch.unique(all_labels)).tolist()
     labels_number = len(labels_unique)
     print('Number of labels/classes: %d - which are %s' % (labels_number, labels_unique))
-    # TODO: We lose the information about the category (by indexing?) 
-    # => Have to track the real categories for later processing
 
+    ## Specificity
+    from sklearn.metrics import confusion_matrix
+    resulting_confusion_matrix = confusion_matrix(all_labels, all_predictions)
+    FP = resulting_confusion_matrix.sum(axis=0) - np.diag(resulting_confusion_matrix)  
+    FN = resulting_confusion_matrix.sum(axis=1) - np.diag(resulting_confusion_matrix)
+    TP = np.diag(resulting_confusion_matrix)
+    TN = resulting_confusion_matrix.sum() - (FP + FN + TP)
 
-    # Torchmetrics required by task description
-    # https://torchmetrics.readthedocs.io/en/stable/classification/recall.html
-    # https://torchmetrics.readthedocs.io/en/stable/classification/precision.html
-    # https://torchmetrics.readthedocs.io/en/stable/classification/f1_score.html
+    FP = sum(FP)
+    FN = sum(FN)
+    TP = sum(TP)
+    TN = sum(TN)
 
-    # Accuracy for binary classification # TODO: Accuracy is not necessary
-    
-    # Accuracy for multi-class classification
-    accuracy = Accuracy(task='multiclass', num_classes=labels_number)
-    acc = accuracy(all_predictions, all_labels)
-    print('Accuracy of the model on the test data:  %f' % (acc))
-    
-    # Calculate Recall
-    # TODO: For binary classification
-    # TODO: For multi-class classification
-    recall = Recall(task='multiclass', num_classes=labels_number)
-    rec = recall(all_predictions, all_labels)
-    print('Recall of the model on the test data: \t %f' % (rec))
-    
-    recallMacro = Recall(task='multiclass', average='macro', num_classes=labels_number)
-    rec = recallMacro(all_predictions, all_labels)
-    print('Recall MACRO of the model on the test data: \t %f' % (rec))
-    
-    recallMicro = Recall(task='multiclass', average='micro', num_classes=labels_number)
-    rec = recallMicro(all_predictions, all_labels)
-    print('Recall MICRO of the model on the test data: \t %f' % (rec))
-    
-    # Calculate Precision
-    # TODO: For binary classification
-    # TODO: For multi-class classification
-    precision = Precision(task='multiclass', num_classes=labels_number)
-    pre = precision(all_predictions, all_labels)
-    print('Precision of the model on the test data: %f' % (pre))
-    
-    precisionMacro = Precision(task='multiclass', average='macro', num_classes=labels_number)
-    pre = precisionMacro(all_predictions, all_labels)
-    print('Precision MACRO of the model on the test data:\t %f' % (pre))
-    
-    precisionMicro = Precision(task='multiclass', average='micro', num_classes=labels_number)
-    pre = precisionMicro(all_predictions, all_labels)
-    print('Precision MICRO of the model on the test data:\t %f' % (pre))
-        
-    # Calculate F1-Scores
-    # TODO: For binary classification
-    # TODO: For multi-class classification    
-    f1score = F1Score(task='multiclass', num_classes=labels_number)
-    f1s = f1score(all_predictions, all_labels)
-    print('F1-Score of the model on the test data:\t %f' % (f1s))
+    print(f"fp: {FP}\tfn: {FN}\ttp: {TP} \ttn{TN}")
 
-    f1scoreMacro = F1Score(task='multiclass', average='macro', num_classes=labels_number)
-    f1s = f1scoreMacro(all_predictions, all_labels)
-    print('F1-Score MACRO of the model on the test data:\t %f' % (f1s))
-    
-    f1scoreMicro = F1Score(task='multiclass', average='micro', num_classes=labels_number)
-    f1s = f1scoreMicro(all_predictions, all_labels)
-    print('F1-Score MICRO of the model on the test data:\t %f' % (f1s))
-    
-    f1scoreWeighted = F1Score(task='multiclass', average='weighted', num_classes=labels_number)
-    f1s = f1scoreWeighted(all_predictions, all_labels)
-    print('F1-Score WEIGHTED of the model on the test data: %f' % (f1s))
-    
-    #f1scoreNone = F1Score(task='multiclass', average='none', num_classes=labels_number)
-    #f1s = f1scoreNone(all_predictions, all_labels)
-    #print('F1-Score NONE of the model on the test data:\t %f' % (f1s))
-    # TODO: average='none' results in
-    # ValueError: only one element tensors can be converted to Python scalars
-    #
-    # Documentation states:
-    # average ... Defines the reduction that is applied over labels. Should be one of the following:
-    # micro: Sum statistics over all labels
-    # macro: Calculate statistics for each label and average them
-    # weighted: Calculates statistics for each label and computes weighted average using their support
-    # "none" or None: Calculates statistic for each label and applies no reduction
-    #
-    
+    # Sensitivity, hit rate, recall, or true positive rate
+    TPR = TP/(TP+FN)
+    print(f"Sensitivity, hit rate, recall, or true positive rate: {TPR}")
+    # Specificity or true negative rate
+    TNR = TN/(TN+FP) 
+    print(f"Specificity or true negative rate: {TNR}")
+    # Precision or positive predictive value
+    PPV = TP/(TP+FP)
+    print(f"Precision or positive predictive value: {PPV}")
+    # Negative predictive value
+    NPV = TN/(TN+FN)
+    print(f"Negative predictive value: {NPV}")
+    # Fall out or false positive rate
+    FPR = FP/(FP+TN)
+    # False negative rate
+    FNR = FN/(TP+FN)
+    # False discovery rate
+    FDR = FP/(TP+FP)
+
+    # Overall accuracy
+    ACC = (TP+TN)/(TP+FP+FN+TN)
+    print(f"Overall accuracy: {ACC}")    
