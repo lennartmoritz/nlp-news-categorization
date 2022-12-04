05f6f0d9b554caf740ea9181d7aad3bfd3dddb74
christianschuler8989 <christianschuler8989@gmail.com>
2022-12-04 10:53:25 +0100

    Evaluations now optionally saved to txt-file and/or displayed on console.

--- news-categorization.py
+++ news-categorization.py
@@ -157,7 +157,6 @@ if __name__ == "__main__":
     
     #TODO: check that the datasets are balanced, i.e. all categories must appear
     training_dataset, testing_dataset, validating_dataset = torch.utils.data.random_split(dataset, [train_size, test_size, validate_size])
-    print("Type of training_dataset: ", type(training_dataset))
     
     def check_dataset_balance(this_dataset, intended_num_of_classes) -> bool:
         """
@@ -186,7 +185,7 @@ if __name__ == "__main__":
     #validate_generator = DataLoader(validating_dataset, **params)
 
     learning_rate = 0.01    # How fast the model learns
-    num_epochs = 2          # How often the model walks through the data
+    num_epochs = 10          # How often the model walks through the data
 
     # Network Parameters
     hidden_size = 100  # 1st layer and 2nd layer number of features
@@ -225,9 +224,9 @@ if __name__ == "__main__":
         avg_loss = avg_loss / total_batch
         print('Finished epoch [%d/%d], Average loss: %.5f, Train-Size: %d, Test-Size: %d' % (epoch + 1, num_epochs, avg_loss, train_size, test_size))
 
-    # Save model after training
-    # TODO: Careful! This gets big really fast!
+    # Save model after training TODO: Careful! This gets big really fast!
     save_model=False
+    
     if save_model:
         os.makedirs("./models", exist_ok=True)
         news_net.to("cpu")
@@ -267,68 +266,81 @@ if __name__ == "__main__":
     TP = np.diag(resulting_confusion_matrix)
     TN = resulting_confusion_matrix.sum() - (FP + FN + TP)
 
-    # Evaluation on scope of class level
-    print("Evaluation on scope of class level")
-    print(f"FP: {FP}     FN: {FN}     TP: {TP}     TN: {TN}")
 
-    # Sensitivity, hit rate, recall, or true positive rate
-    TPR = TP/(TP+FN)
-    print(f"{'Sensitivity, hit rate, recall, or true positive rate:':<55}{TPR}")
-    # Specificity or true negative rate
-    TNR = TN/(TN+FP) 
-    print(f"{'Specificity or true negative rate:':<55}{TNR}")
-    # Precision or positive predictive value
-    PPV = TP/(TP+FP)
-    print(f"{'Precision or positive predictive value:':<55}{PPV}")
-    # Negative predictive value
-    NPV = TN/(TN+FN)
-    print(f"{'Negative predictive value:':<55}{NPV}")
-    # Fall out or false positive rate
-    FPR = FP/(FP+TN)
-    print(f"{'Fall out or false positive rate:':<55}{FPR}")
-    # False negative rate
-    FNR = FN/(TP+FN)
-    print(f"{'False negative rate:':<55}{FNR}")
-    # False discovery rate
-    FDR = FP/(TP+FP)
-    print(f"{'False discovery rate:':<55}{FDR}")
-    # Overall accuracy
-    ACC = (TP+TN)/(TP+FP+FN+TN)
-    print(f"{'Overall accuracy:':<55}{ACC}") 
+    # === All types of evaluations ===
+    
+    # Evaluation on scope of class level
+    TPR = TP/(TP+FN)    # Sensitivity, hit rate, recall, or true positive rate
+    TNR = TN/(TN+FP)    # Specificity or true negative rate
+    PPV = TP/(TP+FP)    # Precision or positive predictive value
+    NPV = TN/(TN+FN)    # Negative predictive value
+    FPR = FP/(FP+TN)    # Fall out or false positive rate
+    FNR = FN/(TP+FN)    # False negative rate
+    FDR = FP/(TP+FP)    # False discovery rate
+    ACC = (TP+TN)/(TP+FP+FN+TN)    # Overall accuracy
+    evaluations = [FP, FN, TP, TN, TPR, TNR, PPV, NPV, FPR, FNR, FDR, ACC]
     
     # Evaluation on scope above of class level
-    print("Evaluation on scope above of class level")
-    
-    # Summing the values for all classes together
-    FP = sum(FP)
-    FN = sum(FN)
-    TP = sum(TP)
-    TN = sum(TN)
-    
-    print(f"FP: {FP}     FN: {FN}     TP: {TP}     TN: {TN}")
+    FPa = sum(FP)    # Summing the values for all classes together
+    FNa = sum(FN)
+    TPa = sum(TP)
+    TNa = sum(TN)
+    TPRa = TPa/(TPa+FNa)    # Sensitivity, hit rate, recall, or true positive rate
+    TNRa = TNa/(TNa+FPa)    # Specificity or true negative rate
+    PPVa = TPa/(TPa+FPa)    # Precision or positive predictive value
+    NPVa = TNa/(TNa+FNa)    # Negative predictive value
+    FPRa = FPa/(FPa+TNa)    # Fall out or false positive rate
+    FNRa = FNa/(TPa+FNa)    # False negative rate
+    FDRa = FPa/(TPa+FPa)    # False discovery rate
+    ACCa = (TPa+TNa)/(TPa+FPa+FNa+TNa)    # Overall accuracy
+    evaluationsa = [FPa, FNa, TPa, TNa, TPRa, TNRa, PPVa, NPVa, FPRa, FNRa, FDRa, ACCa]
 
-    # Sensitivity, hit rate, recall, or true positive rate
-    TPR = TP/(TP+FN)
-    print(f"{'Sensitivity, hit rate, recall, or true positive rate:':<55}{TPR:>12}")
-    # Specificity or true negative rate
-    TNR = TN/(TN+FP) 
-    print(f"{'Specificity or true negative rate:':<55}{TNR:>12}")
-    # Precision or positive predictive value
-    PPV = TP/(TP+FP)
-    print(f"{'Precision or positive predictive value:':<55}{PPV:>12}")
-    # Negative predictive value
-    NPV = TN/(TN+FN)
-    print(f"{'Negative predictive value:':<55}{NPV:>12}")
-    # Fall out or false positive rate
-    FPR = FP/(FP+TN)
-    print(f"{'Fall out or false positive rate:':<55}{FPR:>12}")
-    # False negative rate
-    FNR = FN/(TP+FN)
-    print(f"{'False negative rate:':<55}{FNR:>12}")
-    # False discovery rate
-    FDR = FP/(TP+FP)
-    print(f"{'False discovery rate:':<55}{FDR:>12}")
-    # Overall accuracy
-    ACC = (TP+TN)/(TP+FP+FN+TN)
-    print(f"{'Overall accuracy:':<55}{ACC:>12}") 
+    # Save evaluations to file
+    save_eval=True
+
+    if save_eval:
+        os.makedirs("./evaluations", exist_ok=True)
+        current_classes_as_string = ""
+        for class_key in list_of_classes:
+            current_classes_as_string = current_classes_as_string + class_key
+        current_filename = "evaluation-" + current_classes_as_string + "-" + str(num_epochs)
+        current_file = "./evaluations/" + current_filename + ".txt"
+        with open(current_file, 'w') as file:
+            file.write(str(labels_occurences)+"\n")
+            file.write("\n")            
+            for element in evaluations:
+                file.write(str(element)+"\n")
+            file.write("\n")
+            for element in evaluationsa:
+                file.write(str(element)+"\n")
+
+    # Display evaluations on console
+    display_eval=False
+
+    if display_eval:
+        # Evaluation on scope of class level
+        print("Evaluation on scope of class level")
+        print(f"FP: {FP}     FN: {FN}     TP: {TP}     TN: {TN}")
+
+        print(f"{'Sensitivity, hit rate, recall, or true positive rate:':<55}{TPR}")
+        print(f"{'Specificity or true negative rate:':<55}{TNR}")
+        print(f"{'Precision or positive predictive value:':<55}{PPV}")
+        print(f"{'Negative predictive value:':<55}{NPV}")
+        print(f"{'Fall out or false positive rate:':<55}{FPR}")
+        print(f"{'False negative rate:':<55}{FNR}")
+        print(f"{'False discovery rate:':<55}{FDR}")
+        print(f"{'Overall accuracy:':<55}{ACC}") 
+        
+        # Evaluation on scope above of class level
+        print("Evaluation on scope above of class level")
+        print(f"FP: {FPa}     FN: {FNa}     TP: {TPa}     TN: {TNa}")
+
+        print(f"{'Sensitivity, hit rate, recall, or true positive rate:':<55}{TPRa:>12}")
+        print(f"{'Specificity or true negative rate:':<55}{TNRa:>12}")
+        print(f"{'Precision or positive predictive value:':<55}{PPVa:>12}")
+        print(f"{'Negative predictive value:':<55}{NPVa:>12}")
+        print(f"{'Fall out or false positive rate:':<55}{FPRa:>12}")
+        print(f"{'False negative rate:':<55}{FNRa:>12}")
+        print(f"{'False discovery rate:':<55}{FDRa:>12}")
+        print(f"{'Overall accuracy:':<55}{ACCa:>12}") 
 
